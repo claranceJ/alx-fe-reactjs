0392c1f413175f8cdad9b4603a44d12bb0b2553e
claranceJ <jacktonec@gmail.com>
2024-08-16 15:14:40 +0300

    feat: Added new Count component with increment and dicrement functionalities. imported the new component into the App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,8 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,6 +20,7 @@ function App() {
         <h1>UserProfile</h1>
         <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       </div>
+      <Counter />
       <UserProfile />      
       <Header />
       <MainContent />
