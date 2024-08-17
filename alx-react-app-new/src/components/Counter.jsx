6d5ffc71a9edd17e38e8a15c1586c2e983bd7ee8
claranceJ <jacktonec@gmail.com>
2024-08-17 11:51:34 +0300

    feat: impimented Context API and consumed hooks instead of props

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+
 function Counter() {
     const [count, setCount] = useState(0);
 
---
+++ alx-react-app-props/src/components/UserContext.js
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+const UserContext = React.createContext();
+
+
+export default createContext;
