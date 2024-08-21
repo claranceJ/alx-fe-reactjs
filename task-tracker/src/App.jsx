c65fe3ac9c0d8fb5f8cf8163063887ce9dbec693
claranceJ <jacktonec@gmail.com>
2024-08-21 21:54:21 +0300

    Feat: Added new styling to the Landing page

--- task-tracker/src/App.jsx
+++ task-tracker/src/App.jsx
@@ -4,8 +4,8 @@ import Snackbar from './components/Snackbar';
 
 const App = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="w-96 p-6 rounded-lg shadow-md bg-white text-center">
+    <div className="flex justify-center items-center h-screen bg-neutral-400">
+      <div className="w-102 p-6 rounded-lg shadow-md bg-white text-center">
         <div className="mb-6">
           <h1 className="text-2xl font-semibold">Task Tracker</h1>
         </div>
