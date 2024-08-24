81f7e088830440d2feb7fb37a93efc3932760314
claranceJ <jacktonec@gmail.com>
2024-08-24 08:57:38 +0300

    FEAT: installed zustand for statemanagement and added components

--- recipe-sharing-app/src/components/AddRecipeForm.jsx
+++ recipe-sharing-app/src/components/AddRecipeForm.jsx
@@ -1,6 +1,6 @@
  // AddRecipeForm component
  import { useState } from 'react';
- import { useRecipeStore } from '../store/useRecipeStore';
+ import { useRecipeStore } from '../components/recipeStore';
 
  const AddRecipeForm = () => {
    const addRecipe = useRecipeStore(state => state.addRecipe);
--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,5 @@
  // RecipeList component
- import { useRecipeStore } from '../store/useRecipeStore';
+ import { useRecipeStore } from '../components/recipeStore';
 
  const RecipeList = () => {
    const recipes = useRecipeStore(state => state.recipes);
