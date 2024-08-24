4a6919ba9b38bcb8a770769968e64bb1651d3fea
claranceJ <jacktonec@gmail.com>
2024-08-24 14:37:05 +0300

    FEAT: Create a SearchBar component that allows users to input a search term. This component should update the search term in the Zustand store on change.

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,6 +1,7 @@
 // src/App.jsx
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeList from "./components/RecipeList";
 import RecipeDetails from "./components/RecipeDetails";
 import SearchBar from "./components/SearchBar";
@@ -11,6 +12,7 @@ const App = () => {
       <SearchBar />
       <Routes>
         <Route path="/" element={<RecipeList />} />
+        <Route path="add-recipe" element={<AddRecipeForm/>} />
         <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
       </Routes>
     </Router>
