87c184121a598d2304b5f67e280d38766c52a81a
claranceJ <jacktonec@gmail.com>
2024-08-24 08:50:20 +0300

    FEAT: installed zustand for statemanagement and added components

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import AddRecipeForm from './components/AddRecipeForm'
+import RecipeList from './components/RecipeList'
+
+
+function App() {
+
+  return (
+    <>
+      <AddRecipeForm/>
+      <RecipeList/>
+    </>
+  )
+}
+
+export default App
