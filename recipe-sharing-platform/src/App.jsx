18f872e4c81131d22d3a6f9304a13b574ae4127e
claranceJ <jacktonec@gmail.com>
2024-09-14 09:19:56 +0300

    feat: Initiated Routing in the App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import HomePage from './components/HomePage'
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import RecipeDetail from "./components/RecipeDetail";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <HomePage/>
-     
-    </>
-  )
+    // Wrap the entire app in Router for routing functionality
+    <Router>
+      {/* Define the different routes */}
+      <Routes>
+        {/* Home Page route */}
+        <Route path="/" element={<HomePage />} />
+
+        {/* Recipe Detail Page route - uses dynamic routing to match recipe id */}
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
