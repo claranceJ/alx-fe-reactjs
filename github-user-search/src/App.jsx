1e0af2adc3984d529743c48e20cb34edc9cc06e0
claranceJ <jacktonec@gmail.com>
2024-09-22 08:11:02 +0300

    imported search.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import './App.css'
-import SearchUser from './components/SearchUser'
+import Search from './components/Search'
 
 function App() {
 
   return (
     <div>
       <h1>Github user search</h1>
-      <SearchUser/>
+      <Search />
     </div>
     
   )
